674215a12028b4e25ba242850d6e032c71efc619
Julius Smith <julius.smith@gmail.com>
2021-10-30 01:06:03 -0700

    add new files

---
+++ effects/jos_zitarev.cpp
@@ -0,0 +1,33 @@
+/**
+ * jos_zitarev.cpp
+ * Julius Smith
+ * October 2021
+ * MIT License with LGPL component
+ */
+
+#include "jos_zitarev.h"
+
+#include <iostream>
+
+namespace jos {
+
+  void Zitarev::processBlock (juce::AudioBuffer<float>& audioBuffer, juce::MidiBuffer& midi)
+  {
+    if (not inited) { return; }
+    int numSamples = audioBuffer.getNumSamples();
+    jassert(getNumInputs() <= audioBuffer.getNumChannels());
+    jassert(getNumOutputs() <= audioBuffer.getNumChannels());
+    float** writePointers = audioBuffer.getArrayOfWritePointers();
+    float** readPointers { writePointers }; // we process in place - COMPILE FAUST WITH -inpl OPTION!
+    compute(numSamples, readPointers, writePointers);
+  }
+  
+  //*******************************************************************************
+  void Zitarev::compute(int nframes, float** inputs, float** outputs)
+  {
+    jassert(inited);
+    if (mReverbLevel > 0.0f) {
+      zitarevP->compute(nframes, inputs, outputs);
+    } // else in-place processing means we're done
+  }
+};
---
+++ effects/jos_zitarev.h
@@ -0,0 +1,102 @@
+/**
+ * jos_zitarev.h
+ * Julius Smith
+ * October 2021
+ * MIT License with LGPL component
+ */
+
+/** Zitarev from the faustlibraries distribution (in reverbs.lib)
+ *
+ */
+#ifndef __ZITAREV_H__
+#define __ZITAREV_H__
+
+namespace jos {
+
+  //#define SINE_TEST
+
+#include "../FaustModule.h"
+#include "../faust-src/faustheadersdir/zitarev.h" // stereo in and out
+
+//==============================================================================
+/**
+    An echo-based delay effect ("artificial reverberation"), providing a virtual acoustic listening space.
+
+    @tags{Effects}
+*/
+
+  class Zitarev : public jos::FaustModule
+  {
+    int mNumInputs;
+    int mNumOutputs;
+
+    float mReverbLevel;
+
+    zitarev* zitarevP;
+    APIUI* zitarevUIP;
+
+    void compute(int nframes, float** inputs, float** outputs);
+
+  public:
+    //==============================================================================
+    /** Creates an instance of Zitarev with the specified number of input and output channels. */
+    Zitarev(int numInChans, int numOutChans) // xtor
+      : mNumInputs(numInChans), mNumOutputs(numOutChans)
+    {
+      if ( mNumInputs < 1 ) {
+        std::cerr << "*** Zitarev.h: must have at least one input audio channels\n";
+        mNumInputs = 1;
+      }
+      if ( mNumInputs > 2 ) {
+        std::cerr << "*** Zitarev.h: limiting number of audio output channels to 2\n";
+        mNumInputs = 2;
+      }
+#if 1
+      std::cout << "Zitarev: constructed for "
+                << mNumInputs << " input channels and "
+                << mNumOutputs << " output channels with reverb level = "
+                << mReverbLevel << "\n";
+#endif
+
+      zitarevP = new zitarev; // stereo input and output
+      zitarevUIP = new APIUI; // #included in *dsp.h
+      zitarevP->buildUserInterface(zitarevUIP);
+    }
+
+    /** Destructor.
+
+        This will free any memory allocated by Zitarev.
+    */
+    virtual ~Zitarev() {
+      delete zitarevP;
+      delete zitarevUIP;
+    }
+
+    int getNumInputs() override { return(mNumInputs); }
+    int getNumOutputs() override { return(mNumOutputs); }
+
+    void prepareToPlay(double samplingRateHz, int maxSamplesPerBlock) override {
+      DBG("Zitarev: prepareToPlay(" << samplingRateHz << ")");
+      jassert(samplingRateHz>0);
+      FaustModule::prepareToPlay(samplingRateHz, maxSamplesPerBlock); // common initialization, e.g., sampleRate = samplingRateHz;
+      FAUSTFLOAT fs = FAUSTFLOAT(samplingRateHz); // Faust typically uses floats, but may be double or quad
+
+      zitarevP->init(fs); // compression filter parameters depend on sampling rate
+      jassert(zitarevP->getNumOutputs() == mNumOutputs);
+      int ndx = zitarevUIP->getParamIndex("Wet");
+      zitarevUIP->setParamValue(ndx, mReverbLevel);
+    }
+
+    //==============================================================================
+    /** Set the reverberation level between 0 (no reverb) to 1 (maximally "wet" reverb) */
+    void setReverbLevel(float level) {
+      mReverbLevel = level;
+    }
+
+    virtual void processBlock (juce::AudioBuffer<float>& audio, juce::MidiBuffer& midi) override;
+
+  }; // Class Zitarev
+
+}; // namespace jos
+
+#endif // __ZITAREV_H__
